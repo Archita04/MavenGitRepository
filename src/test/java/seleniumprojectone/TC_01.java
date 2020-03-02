509a5538d8e67538603254988be4ee07ca6b000c
Archita04 <agarwalarchi@gmail.com>
2020-03-02 11:56:44 +0530

    First Commmting

---
+++ src/test/java/seleniumprojectone/TC_01.java
@@ -0,0 +1,21 @@
+package seleniumprojectone;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_01 {
+	
+	@Test
+	public void Test()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+
+}
